a666183eea59f98ae9895a13970e9134e5cd8058
vcokltfre <vcokltfre@gmail.com>
2020-06-21 00:13:28 +0100

    feat: add insult functionality to insult cog

--- cogs/insult.py
+++ cogs/insult.py
@@ -5,5 +5,15 @@ class Insult(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.group(name="insult")
+    async def insult(self, ctx):
+        if ctx.invoked_subcommand == None:
+            await ctx.channel.send("Invalid usage. Docs coming soon:tm:")
+
+    @insult.command(name="fucc")
+    async def fucc(self, ctx, *name):
+        name = " ".join(name)
+        await ctx.channel.send(f"{ctx.author.name} says \"fuck you\", {name}")
+
 def setup(bot):
     bot.add_cog(Insult(bot))
